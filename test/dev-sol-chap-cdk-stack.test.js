80712d3d767a7be232e359d65d3597d0b4ff79e9
johnraphael02 <john.c@strastan.com>
2025-02-25 09:39:46 +0800

    Initial commit

---
+++ test/dev-sol-chap-cdk-stack.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const DevSolChapCdkStack = require('../lib/dev-sol-chap-cdk-stack-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dev-sol-chap-cdk-stack-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new DevSolChapCdkStack.DevSolChapCdkStackStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
